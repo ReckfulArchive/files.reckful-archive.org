00cf509fb4c22a57c7cf658f2f09ad8e489fed68
IgnatBeresnev <ignat.beresnev@jetbrains.com>
2023-06-04 19:48:23 +0200

    Add build scripts

--- build.sh
+++ build.sh
@@ -1,2 +1,21 @@
 #!/bin/bash
 
+pageFile=$1
+assetDir=$2
+remotePath="$3"
+
+pageContent="$(<"$pageFile")"
+
+pageContent="${pageContent//\"$assetDir\//"$remotePath/assets/"}"
+
+listingStartTag="<!-- NGINX_LISTING_START -->"
+listingEndTag="<!-- NGINX_LISTING_END -->"
+
+topContent="${pageContent%$listingStartTag*}"
+bottomContent="${pageContent#*$listingEndTag}"
+
+mkdir -p ./build/assets
+
+echo "$topContent" > ./build/top.html
+echo "$bottomContent" > ./build/bottom.html
+cp -a "$assetDir/." ./build/assets/
